7b2d80a1d678092f6874d4bfb833cac65b05e450
Adrian Harwood <adrian.harwood@manchester.ac.uk>
2020-03-30 12:58:23 +0100

    Initial commit

---
+++ ShellBugs/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ShellBugs.Services;
+using ShellBugs.Views;
+
+namespace ShellBugs
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+            
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override async void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ShellBugs/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ShellBugs.Models;
+
+namespace ShellBugs.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
